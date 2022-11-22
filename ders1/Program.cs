1a50388744469550f5c28364bc213fffbe0c7b97
oztur <oztur@YIIT>
2022-11-22 12:05:54 +0100

    Proje dosyası ekle.

---
+++ ders1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Welcome To Calculator");
+Console.WriteLine("Enter A Number");
+float number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("You Entered " + number);
+
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("Odd");
+    }
+    else
+    {
+        Console.WriteLine("Even");
+    }
+
+
+    
+
